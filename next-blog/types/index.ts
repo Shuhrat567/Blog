02f4960b9c1f7a0a3d414d6914479fcb3827df9f
shuhrat <isoilovshuhrat567@gmail.com>
2024-04-18 17:40:39 +0500

    new change front and back

---
+++ next-blog/types/index.ts
@@ -0,0 +1,29 @@
+export interface ChildProps {
+    children: React.ReactNode
+}
+
+export interface IBlog {
+    title: string
+    description: string
+    author: IAuthor
+    category: ICategoryAndTags
+    tag: ICategoryAndTags
+    image: {
+        url: string
+    }
+    createdAt: string
+    content: {
+        html: string
+    }
+
+}
+export interface IAuthor {
+    name: string
+    image: {
+        url: string
+    }
+}
+export interface ICategoryAndTags {
+    name: string
+    slug: string
+}
